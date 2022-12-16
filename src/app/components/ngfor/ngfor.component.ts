fa176e2fe0af15356a15c4f588e4e7536ac87edb
João Amendola <mx.joaoo@gmail.com>
2022-12-16 19:19:10 -0300

    Resolve exercício 3

--- src/app/components/ngfor/ngfor.component.ts
+++ src/app/components/ngfor/ngfor.component.ts
@@ -8,23 +8,23 @@ import { Component } from '@angular/core';
 export class NgforComponent {
   arrayFotos: any = [
     {
-      url: "src/assets/tanya.png",
+      url: "assets/tanya.png",
       description:"Tanya McQuoid-Hunt (Jennifer Coolidge) from 'The White Lotus' Season 2"
     },
     {
-      url: "src/assets/harper.png",
+      url: "assets/harper.png",
       description:"Harper Spiller (Aubrey Plaza) from 'The White Lotus' Season 2"
     },
     {
-      url: "src/assets/portia.png",
+      url: "assets/portia.png",
       description:"Portia (Harley Lu Richardson) from 'The White Lotus' Season 2"
     },
     {
-      url: "src/assets/ethan.png",
+      url: "assets/ethan.png",
       description:"Ethan Spiller (Will Sharpe) from 'The White Lotus' Season 2"
     },
     {
-      url: "src/assets/cam.png",
+      url: "assets/cam.png",
       description:"Cameron Sullivan (Theo James) from 'The White Lotus' Season 2"
     }
   ]
